988e0c5a9fcc9b6a66c3638b5d0623a17acfa86e
DanielAD10 <adamsdaniel171@gmail.com>
2023-11-16 00:39:56 +0630

    Update in index.html

--- timer.js
+++ timer.js
@@ -5,10 +5,20 @@ form.onsubmit = function(event) {
     event.preventDefault();
 
     let countdown = document.getElementById("countdown").value;
+    const birthDate = new Date(countdown);
+    // console.log(birthDate);
+    const currentDate = new Date();
+    console.log(currentDate);
+
+    if (birthDate == currentDate) {
+        console.log('Happy Birthday');
+    } else {
+        console.log('Your Birthday is coming soon');
+    }
 
     const countDownTimer = setInterval(() => {
-        countdownDisplay.innerHTML = countdown;
-        countdown--;
+        // countdownDisplay.innerHTML = countdown;
+        
 
         if(countdown < 0) {
             clearInterval(countDownTimer);
